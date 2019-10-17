78797499b17c14ec7693c08226bed05c5917f02d
jomar.cardoso <jomar.cardoso@cwi.com.br>
2019-10-16 21:41:57 -0300

    prism

---
+++ gatsby-browser.js
@@ -0,0 +1,1 @@
+require("./static/css/vendors/prism-twilight.css")
--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,22 @@ module.exports = {
         name: `markdown-pages`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+            },
+          }
+        ]
+      }
+    },
     `gatsby-plugin-mdx`,
   ]
 }
